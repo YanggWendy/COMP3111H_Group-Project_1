284aa3adfa038c4d1100c505d09ca80a87aa4843
iMaxmoe <maxmoe0105@gmail.com>
2020-11-11 18:55:39 +0800

    Reformat the code.

--- src/main/java/comp3111/popnames/AnalyzeNames.java
+++ src/main/java/comp3111/popnames/AnalyzeNames.java
@@ -46,52 +46,52 @@ public class AnalyzeNames {
 		return oReport;
 	}
 	
-	
-	 public static int getRank(int year, String name, String gender) {
-	     boolean found = false;
-	     int oRank = 0;
-	 	int rank = 1;
-	     for (CSVRecord rec : getFileParser(year)) {
-	         // Increment rank if gender matches param
-	         if (rec.get(1).equals(gender)) {
-	             // Return rank if name matches param
-	             if (rec.get(0).equals(name)) {
-	             	found = true;
-	             	oRank = rank;
-	             	break;
-	             }
-	             rank++;
-	         }
-	     }
-	     if (found)
-	     	return oRank;
-	     else
-	     	return -1;
-	 }
-	 
- 
-	 public static String getName(int year, int rank, String gender) {
-	 	boolean found = false;
-	     String oName = "";
-	     int currentRank = 0;
-	     
-	     // For every name entry in the CSV file
-	     for (CSVRecord rec : getFileParser(year)) {
-	         // Get its rank if gender matches param
-	         if (rec.get(1).equals(gender)) {
-	             // Get the name whose rank matches param
-	         	currentRank++;
-	            if (currentRank == rank) {
-	             	found = true;
-	             	oName = rec.get(0);
-	                break;
+	public static int getRank(int year, String name, String gender) {
+	    boolean found = false;
+	    int oRank = 0;
+		int rank = 1;
+	    for (CSVRecord rec : getFileParser(year)) {
+	        // Increment rank if gender matches param
+	        if (rec.get(1).equals(gender)) {
+	            // Return rank if name matches param
+	            if (rec.get(0).equals(name)) {
+	            	found = true;
+	            	oRank = rank;
+	            	break;
 	            }
-	         }
-	     }     
-	     if (found)
-	     	return oName;
-	     else
-	     	return "information on the name at the specified rank is not available";
-	 }
+	            rank++;
+	        }
+	    }
+	    if (found)
+	    	return oRank;
+	    else
+	    	return -1;
+	}
+
+	public static String getName(int year, int rank, String gender) {
+		boolean found = false;
+	    String oName = "";
+	    int currentRank = 0;
+	    
+	    // For every name entry in the CSV file
+	    for (CSVRecord rec : getFileParser(year)) {
+	        // Get its rank if gender matches param
+	        if (rec.get(1).equals(gender)) {
+	            // Get the name whose rank matches param
+	        	currentRank++;
+	           if (currentRank == rank) {
+	            	found = true;
+	            	oName = rec.get(0);
+	               break;
+	           }
+	        }
+	    }     
+	    if (found)
+	    	return oName;
+	    else
+	    	return "information on the name at the specified rank is not available";
+	}
+	
+
  
 }
