0d8bdad4c46e2c7b23856ddf4056ebd324735ddf
KikiDMW <yizhezhao@foxmail.com>
2020-11-25 12:37:39 +0800

    Implement additional feature, similar name search

--- src/main/java/comp3111/popnames/PredicReport.java
+++ src/main/java/comp3111/popnames/PredicReport.java
@@ -1,5 +1,12 @@
 package comp3111.popnames;
-import java.util.Random;
+import org.apache.commons.csv.CSVRecord;
+
+import jdk.dynalink.beans.StaticClass;
+
+import javafx.util.Pair;
+
+import java.util.*;
+
 
 
 public class PredicReport {
@@ -92,7 +99,7 @@ public class PredicReport {
 
 	
 	
-	//Task5 Recommendation	
+	//Task6 score prediction	
 		public static String recomendName_task6(String Name, String mateName, int YOB, String gender, String mategender, String prefer) {
 			
 			Random rand = new Random();
@@ -147,5 +154,66 @@ public class PredicReport {
 			return Recommendation;
 		}
 	
+		// Additional feature: Similar name recommendation
+		private static int minDistance(String word1, String word2) {
+	        int m = word1.length(), n = word2.length();
+	        int[][] dp = new int[m+1][n+1];
+	        for (int i = 0; i <= m; ++i) dp[i][0] = i;
+	        for (int i = 0; i <= n; ++i) dp[0][i] = i;
+	        for (int i = 1; i <= m; ++i) {
+	            for (int j = 1; j <= n; ++j) {
+	                if (word1.charAt(i-1) == word2.charAt(j-1)) {
+	                    dp[i][j] = dp[i-1][j-1];
+	                } else {
+	                    dp[i][j] = Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1])) + 1;
+	                }
+	            }
+	        }
+	        return dp[m][n];
+	    }
+		
+		public static List<Pair<String, Integer>> similarNames(String Name, String gender, int popularity){
+			int year0 = 2000, year1 = 2019;
+			PriorityQueue<Pair<String,Integer> > editDistance=
+	                new PriorityQueue<Pair<String,Integer>>(Comparator.comparing(Pair::getValue));
+			Set<String> nameSet = new HashSet<>();
+			for(int year = year0; year <= year1; year++) {
+				int rankStart, rankEnd;
+				int totalRank = AnalyzeNames.get_total_Rank(year, gender);
+				switch(popularity) {
+				case 0:
+					rankStart = 1;
+					rankEnd = (int) (totalRank * 0.3);
+					break;
+				case 1:
+					rankStart = (int) (totalRank * 0.3) + 1;
+					rankEnd = (int) (totalRank * 0.7);
+					break;
+				case 2:
+				default:
+					rankStart = (int) (totalRank * 0.7) + 1;
+					rankEnd = totalRank;
+				}
+				int rank = 0;
+				for (CSVRecord rec: AnalyzeNames.getFileParser(year)) {
+					if (rec.get(1).equals(gender)) {
+						rank++;
+						if(rank < rankStart) continue;
+						if(rank > rankEnd) break;
+						String name = rec.get(0);
+						if(nameSet.contains(name)) continue;
+						int distance = minDistance(name, Name);
+						Pair<String, Integer> pair = new Pair<String, Integer>(name, distance);
+						editDistance.offer(pair);
+						nameSet.add(name);
+					}
+				}
+			}
+			List<Pair<String, Integer>> returnList = new ArrayList<>();
+			for(int i = 1; i <= 10; i++) {
+				returnList.add(editDistance.poll());
+			}
+			return returnList;
+		}
 	
 }
